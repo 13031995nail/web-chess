32ef2da833e2921aef2cd0a92728dd6af4bda06b
13031995nail <valiullin-nail@list.ru>
2019-03-07 14:23:31 +0500

    'ff'

--- hello/static/chess.js
+++ hello/static/chess.js
@@ -109,8 +109,8 @@ function isBlackSquare(coord){
 function progress(position1, position2){
     start = arr[position1%8]+(8-(Math.floor(position1/8)));
     end = arr[position2%8]+(8-(Math.floor(position2/8)));
-	document.location.href = "https://hidden-harbor-40615.herokuapp.com/" + start + end;
+	document.location.href = "https://web-chess-net.herokuapp.com/" + start + end;
 }
 function start_game(){
-	document.location.href = "https://hidden-harbor-40615.herokuapp.com/start";
+	document.location.href = "https://web-chess-net.herokuapp.com/start";
 }
